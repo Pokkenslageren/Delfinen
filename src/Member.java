afa24763bffa0fcaba87a14d3e58873fbbf36ed1
JullerMus <juliusclykkegaard@gmail.com>
2024-05-01 19:47:38 +0200

    changes

--- src/Member.java
+++ src/Member.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
 public class Member{
-	int memberPrice;
+	double memberPrice;
 
 	public Member(int age){
 		if(age < 18){
-			memberPrice = 1000;
+			memberPrice = 1000.0;
 		}
 		else if(age > 60){
-			memberPrice = 1600 * 0.75;
+			memberPrice = (1600 * 0.75);
 		}
 		else{
-			memberPrice = 1600;
+			memberPrice = 1600.0;
 		}
 	}
 
